a9e7a2f0a41fe9c24652e50060d30dd3853f2622
Dan Sinensky <dan.i.sinensky@gmail.com>
2023-08-28 14:40:15 -0400

    sets up database

---
+++ create-database.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE you_and_meme_backend;
+
+CREATE USER you_and_meme_backend_admin WITH PASSWORD 'password';
+
+GRANT ALL PRIVILEGES ON DATABASE you_and_meme_backend TO you_and_meme_backend_admin;
