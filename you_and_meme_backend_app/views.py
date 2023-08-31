c18742817c3db465148f98ebd301108fcf16bdbe
manfredjoa <joamanfred@gmail.com>
2023-08-31 11:37:10 -0400

    add post comment

--- you_and_meme_backend_app/views.py
+++ you_and_meme_backend_app/views.py
@@ -5,26 +5,13 @@ from .models import Profile, Post, Comment, Meme
 
 
 from rest_framework import generics, status, permissions
-# from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
-# from rest_framework_simplejwt import views as jwt_views
-
-
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
-# class LoginView(generics.ListCreateAPIView):
 class LoginView(generics.CreateAPIView):
-    """
-    POST user/login/
-    """
-
-    # This permission class will overide the global permission class setting
-    # Permission checks are always run at the very start of the view, before any other code is allowed to proceed.
-    # The permission class here is set to AllowAny, which overwrites the global class to allow anyone to have access to login.
-    # permission_classes = [IsAdminUser]
     permission_classes = (permissions.AllowAny,)
     serializer_class = UserSerializer
     queryset = User.objects.all()
@@ -34,12 +21,9 @@ class LoginView(generics.CreateAPIView):
         password = request.data.get("password", "")
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            # login saves the user’s ID in the session,
-            # using Django’s session framework.
             login(request, user)
             refresh = RefreshToken.for_user(user)
             serializer = TokenSerializer(data={
-                # using DRF JWT utility functions to generate a token
                 "token": str(refresh.access_token)
             })
             serializer.is_valid()
@@ -47,12 +31,7 @@ class LoginView(generics.CreateAPIView):
         return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
-# class RegisterUsersView(generics.ListCreateAPIView):
 class RegisterUsersView(generics.CreateAPIView):
-    """
-    POST user/signup/
-    """
-    # permission_classes = [IsAdminUser]
     permission_classes = (permissions.AllowAny,)
     serializer_class = UserSerializer
     queryset = User.objects.all()
@@ -74,25 +53,15 @@ class RegisterUsersView(generics.CreateAPIView):
         return Response(status=status.HTTP_201_CREATED)
 
 
-# class CustomTokenObtainPairView(jwt_views.TokenObtainPairView):
-#     permission_classes = [IsAdminUser]
-
-
-# class CustomTokenRefreshView(jwt_views.TokenRefreshView):
-#     permission_classes = [IsAdminUser]
-
-
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    # permission_classes = [IsAdminUser]
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (permissions.AllowAny,)
-    # permission_classes = [IsAdminUser]
 
     def post(self, request, *args, **kwargs):
         meme = request.data.get("meme", "")
@@ -112,10 +81,23 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    # permission_classes = [IsAdminUser]
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        body = request.data.get("body", "")
+        if not body:
+            return Response(
+                data={
+                    "message": "Text body is required to create a comment."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        new_comment = Comment.objects.create_comment(
+            body=body
+        )
+        return Response(status=status.HTTP_201_CREATED)
 
 
 class MemeViewSet(viewsets.ModelViewSet):
     queryset = Meme.objects.all()
     serializer_class = MemeSerializer
-    # permission_classes = [IsAdminUser]
